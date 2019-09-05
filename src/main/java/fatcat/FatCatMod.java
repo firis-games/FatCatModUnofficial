4ff6b2ea3051013308435d27d70c0ec8936b94b9
firis <firis.games.lapismoonlight@gmail.com>
2019-09-05 21:24:23 +0900

    mc1.12.2-ver1.0.3.b

--- src/main/java/fatcat/FatCatMod.java
+++ src/main/java/fatcat/FatCatMod.java
@@ -50,7 +50,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @EventBusSubscriber
 public class FatCatMod {
     public static final String MODID = "fatcat";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.3.b";
 	public static final int STATUS_GUI_ID = 0;
 
 	public static Logger logger;
